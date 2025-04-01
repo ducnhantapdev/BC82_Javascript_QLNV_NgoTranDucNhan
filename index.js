2ba3be5fa43e26f0b9ba230eacf26f397f12867f
ducnhantapdev <ducnhantapdev@gmail.com>
2025-04-01 22:50:35 +0700

    update CRUD

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ function handleTinhLuongTheoChucVu(cv, lcb) {
   switch (cv) {
     case 1:
       tongluong = lcb * 3;
+      break;
     case 2:
       tongluong = lcb * 2;
+      break;
     case 3:
       tongluong = lcb * 1;
+      break;
     default:
       break;
   }
@@ -60,11 +63,35 @@ function handleXepLoaiNhanVien(timeWork) {
   }
 }
 
+//Tim Chuc vu
+function handleTimChucVu(cv) {
+  let tencv = null;
+  switch (cv) {
+    case 1:
+      tencv = "Sếp";
+      break;
+    case 2:
+      tencv = "Trưởng phòng";
+      break;
+    case 3:
+      tencv = "Nhân viên";
+      break;
+    default:
+      break;
+  }
+  return tencv;
+}
+
 function renderNhanVien(arr = arrNhanVien) {
   let content = "";
   for (let nhanVien of arr) {
     let { tk, name, email, ngaylam, luongCB, chucvu, gioLam } = nhanVien;
+    // Chuyển đổi luongCB thành số
+    luongCB = parseFloat(luongCB);
+    chucvu = parseFloat(chucvu);
     let tongluong = handleTinhLuongTheoChucVu(chucvu, luongCB);
+    let cv = handleTimChucVu(chucvu);
+
     console.log(tongluong);
     let loaiNhanVien = null;
     if (handleXepLoaiNhanVien(gioLam) === 1) {
@@ -82,17 +109,63 @@ function renderNhanVien(arr = arrNhanVien) {
         <td>${name}</td>
         <td>${email}</td>
         <td>${ngaylam}</td>
-        <td>${chucvu}</td>
+        <td>${cv}</td>
         <td>${tongluong}</td>
         <td>${loaiNhanVien}</td>
+        <td class="d-flex col-12 ">
+          <button
+              class="btn btn-primary mr-2"
+              id="btnSua"
+              data-toggle="modal"
+              data-target="#myModal"
+              onclick="handleUpdateNhanVien('${email}')"
+          >
+                    Sửa
+          </button>
+        <button class="btn btn-danger btnXoa" onclick="handleDeleteNhanVien('${email}')">Xoá</button>
+        
+        </td>
     </tr>
     `;
   }
 
   document.querySelector("#tableDanhSach").innerHTML = content;
 }
+let emailUpdate = null;
+function handleUpdateNhanVien(email) {
+  return (emailUpdate = email);
+}
+document.querySelector("#btnCapNhat").onclick = function updateNhanVien() {
+  let form = document.querySelector("#formNhanVien");
+  let nhanvien = layDuLieuTuForm(form);
+  console.log(nhanvien);
+  let email = handleUpdateNhanVien(emailUpdate);
 
-function handleAddNhanVien() {}
-function handleUpdateNhanVien() {}
-function handleDeleteNhanVien() {}
-function handleSearchNhanVien() {}
+  let nhanvienindex = arrNhanVien.findIndex((item) => item.email === email);
+  console.log(nhanvienindex);
+
+  if (nhanvienindex != -1) {
+    arrNhanVien[nhanvienindex] = nhanvien;
+    saveDataNhanVienLocal();
+    renderNhanVien();
+  }
+};
+
+// nhanvienUpdate.tk = document.querySelector("#tknv").value;
+// nhanvienUpdate.name = document.querySelector("#name").value;
+// nhanvienUpdate.email = document.querySelector("#email").value;
+// nhanvienUpdate.password = document.querySelector("#password").value;
+// nhanvienUpdate.ngaylam = document.querySelector("#datepicker").value;
+// nhanvienUpdate.luongCB = document.querySelector("#luongCB").value;
+// nhanvienUpdate.chucvu = document.querySelector("#chucvu").value;
+// nhanvienUpdate.gioLam = document.querySelector("#gioLam").value;
+
+let emailDelete = null;
+function handleDeleteNhanVien(email) {
+  let nhanVienCanXoa = arrNhanVien.findIndex((item) => item.email === email);
+  if (nhanVienCanXoa != -1) {
+    arrNhanVien.splice(nhanVienCanXoa, 1);
+    saveDataNhanVienLocal();
+    renderNhanVien();
+  }
+}
